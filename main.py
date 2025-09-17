e3529c7b17a946f639216c101552eba56342f926
shashank <24f1000119@ds.study.iitm.ac.in>
2025-09-17 22:56:23 +0530

    Implement sum functionality

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from sum import add
+
 def welcome():
     print("Welcome to the Command-Line Calculator!")
 
 if __name__ == "__main__":
     welcome()
+    sum_result = add(20, 10)
+    print(f"20 + 10 = {sum_result}")
